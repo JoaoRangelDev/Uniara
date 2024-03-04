5a38024c3c2a60e34d3fb0cedbdd3f54fa925361
JoaoRangelDev <joao.rangel@simples.software>
2024-03-04 11:56:33 -0300

    Novos Exercicios

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,39 @@ import 'package:trabalhoandre/services/Exercicios.dart';
 
 main(List<String> arguments){
   Exercicios exercicios = Exercicios();
-  int fatorial = exercicios.fatorial(num: 2);
-  int somaNumerosPares = exercicios.somaNumerosPares(num: 450);
-  bool numeroPrimo = exercicios.verificaPrimo(num: 101);
-  double imc = exercicios.calculaIMC(altura: 1.80, peso: 78);
-  List<int> listTabuada = exercicios.tabuada(num: 10);
-  bool palindromo = exercicios.verificaPalindromo(palavra: "ovo");
-  double temp = exercicios.temperaturaCelsiusParaFahrenheit(temp: 35);
 
+  // Exercicios da AULA 3
+  int fatorial = exercicios.fatorial(num: 2);
+  print("Fatorial: $fatorial\n");
+
+  int somaNumerosPares = exercicios.somaNumerosPares(num: 450);
+  print("Soma dos numeros pares é: $somaNumerosPares\n");
+
+  bool numeroPrimo = exercicios.verificaPrimo(num: 101);
+  print(numeroPrimo ? 'É PRIMO\n' : "Não é PRIMO\n");
+
+  double imc = exercicios.calculaIMC(altura: 1.80, peso: 78);
+  print("IMC: $imc");
+
+  List<int> listTabuada = exercicios.tabuada(num: 10);
+  for(var num in listTabuada){
+    print("Tabuada: $num");
+  }
+
+  bool palindromo = exercicios.verificaPalindromo(palavra: "ovo");
+  print(palindromo ? 'É Palindromo\n' : "Não é Palindromo\n");
+
+  double temp = exercicios.temperaturaCelsiusParaFahrenheit(temp: 35);
+  print("Temperatura Fahrenheit: $temp");
+
+  //Exercicios da Aula 4
+  // exercicios.lerNumeroPINP();
+  // exercicios.lerNumeroSucessorAntecessor();
+  // exercicios.salarioMinimo();
+  //  exercicios.reajusteSalario();
+  // exercicios.trueOrFalse();
+  // exercicios.ordemDescrecente();
+  // exercicios.calculaIMCeMostra();
+  exercicios.media();
+  exercicios.mediaFinal();
 }
--- lib/services/Exercicios.dart
+++ lib/services/Exercicios.dart
@@ -1,5 +1,7 @@
-class Exercicios{
-  int fatorial({required int num}){
+import 'dart:io';
+
+class Exercicios {
+  int fatorial({required int num}) {
     int soma = 0;
     int fat = 1;
 
@@ -11,7 +13,7 @@ class Exercicios{
     return soma;
   }
 
-  int somaNumerosPares({required int num}){
+  int somaNumerosPares({required int num}) {
     int soma = 0;
 
     for (int i = 1; i <= num; i++) {
@@ -21,10 +23,9 @@ class Exercicios{
     }
 
     return soma;
-
   }
 
-  bool verificaPrimo({required int num}){
+  bool verificaPrimo({required int num}) {
     bool primo = true;
 
     for (int i = 2; i * i <= num; i++) {
@@ -35,11 +36,10 @@ class Exercicios{
     }
 
     return primo;
-
   }
 
-  bool verificaPalindromo({required String palavra}){
-    String palavraInv= "";
+  bool verificaPalindromo({required String palavra}) {
+    String palavraInv = "";
 
     for (int i = palavra.length - 1; i >= 0; i--) {
       palavraInv += palavra[i];
@@ -48,34 +48,148 @@ class Exercicios{
     bool ehPalindromo = palavra == palavraInv;
 
     return ehPalindromo;
-
   }
 
-  double temperaturaCelsiusParaFahrenheit({required double temp}){
-    double tempFahrenheit = (temp * 9/5) + 32;
+  double temperaturaCelsiusParaFahrenheit({required double temp}) {
+    double tempFahrenheit = (temp * 9 / 5) + 32;
 
     return tempFahrenheit;
-
   }
 
-  double calculaIMC({required double altura, required double peso}){
+  double calculaIMC({required double altura, required double peso}) {
     double imc = peso / (altura * altura);
 
     return imc;
-
   }
 
-  List<int> tabuada({required int num}){
+  List<int> tabuada({required int num}) {
     List<int> listaTabuada = [];
 
     for (int i = 1; i <= 10; i++) {
-      // Multiplicar o número pelo valor de i
       int resultado = num * i;
 
       listaTabuada.add(resultado);
     }
 
     return listaTabuada;
+  }
+
+  lerNumeroPINP() {
+    print("\nDigite um numero: ");
+    int num = int.parse(stdin.readLineSync()!);
+
+    print(num % 2 == 0 ? "Esse numero é par" : "Esse numero é impar");
+    print(num < 0 ? "Esse numero é negativo" : "Esse numero é positivo");
+  }
+
+  lerNumeroSucessorAntecessor() {
+    print("\nDigite um numero: ");
+    int num = int.parse(stdin.readLineSync()!);
+
+    print("Sucessor é ${num + 1}");
+    print("Antecessor é ${num - 1}");
+  }
+
+  salarioMinimo() {
+    print("\nDigite seu salario: ");
+    double salarioMinimo = 1412;
+    double salarioUsuario = double.parse(stdin.readLineSync()!);
+
+    double salariosMinimos = salarioUsuario / salarioMinimo;
+
+    print("O salario minimo do usuario é: $salariosMinimos");
+  }
+
+  reajusteSalario() {
+    print("\nDigite seu salario: ");
+    double salarioUsuario = double.parse(stdin.readLineSync()!);
+
+    print("Salario com reajuste pra cima: ${salarioUsuario / 0.95}");
+    print("Salario com reajuste pra baixo: ${salarioUsuario * 0.95}");
+  }
+
+  trueOrFalse() {
+    print("Digite o primeiro valor com 'true' ou 'false': ");
+    String? firstBoolString = stdin.readLineSync();
+    bool firstBool = firstBoolString?.toLowerCase() == 'true';
+
+    print("Digite o segundo valor 'true' ou 'false': ");
+    String? secondBoolString = stdin.readLineSync();
+    bool secondBool = secondBoolString?.toLowerCase() == 'true';
+
+    if (firstBool && secondBool) {
+      print("Verdadeiro");
+    } else {
+      print("Falso");
+    }
+  }
+
+  ordemDescrecente() {
+    List<double> lista = [];
+
+    for (var i = 0; i < 3; i++) {
+      print("\nDigite o ${i + 1}º numero: ");
+      double num = double.parse(stdin.readLineSync()!);
+      lista.add(num);
+    }
+
+    lista.sort((a, b) => b.compareTo(a));
+    print(lista);
+  }
+
+  calculaIMCeMostra() {
+    print("\nDigite sua altura: ");
+    double altura = double.parse(stdin.readLineSync()!);
+
+    print("\nDigite seu peso: ");
+    double peso = double.parse(stdin.readLineSync()!);
+
+
+    double imc = peso / (altura * altura);
+
+    if (imc < 18.5) {
+      print("Abaixo do peso");
+    } else if (imc < 24.9) {
+      print("Peso ideal (parabéns)");
+    } else if (imc < 29.9) {
+      print("Levemente acima do peso");
+    } else if (imc < 34.9) {
+      print("Obesidade grau I");
+    } else if (imc < 39.9) {
+      print("Obesidade grau II (severa)");
+    } else {
+      print("Obesidade grau III (mórbida)");
+    }
+  }
+
+  media() {
+    double media = 0;
+
+    for (var i = 0; i < 3; i++) {
+      print("\nDigite a ${i + 1}ª nota: ");
+      double num = double.parse(stdin.readLineSync()!);
+      media += num;
+    }
+    media /= 3;
+
+    print(media);
+  }
+
+  mediaFinal() {
+    double media = 0;
+
+
+    print("Digite o nome do aluno: ");
+    String? nome = stdin.readLineSync();
+
+    for (var i = 0; i < 4; i++) {
+      print("\nDigite a ${i + 1}ª nota: ");
+      double num = double.parse(stdin.readLineSync()!);
+      media += num;
+    }
+    media /= 4;
+
+    print("\nAluno: $nome \nMédia: $media \n${media < 7 ? "REPROVADO!" : "Aprovado!"}");
   }
 
 }
